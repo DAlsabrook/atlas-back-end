4230bea79615daf4c7effaec03db8a5532dc4297
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-14 18:10:13 +0000

    First attempt at task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+First module to work with flask api apps
+"""
+import requests
+import sys
+
+
+class Get_Todo():
+    """class to get employee information
+    """
+
+    def todo(self):
+        args = sys.argv
+        user_id = args[1]
+        url = "https://jsonplaceholder.typicode.com/"
+
+        # Get api to json locally stored
+        user_result = requests.get(url + "users/" + user_id)
+        todos_result = requests.get(url + "todos")
+        user_json = user_result.json()
+        todos_json = todos_result.json()
+
+        # Setting variables
+        EMPLOYEE_NAME = user_json["name"]
+        NUMBER_OF_DONE_TASKS = sum(1 for task in todos_json
+                                   if str(task["userId"]) == user_id
+                                   and task["completed"] == True)
+        TOTAL_NUMBER_OF_TASKS = sum(1 for task in todos_json
+                                    if str(task["userId"]) == user_id)
+
+        # Printing employee info and task titles
+        print(f"Employee {EMPLOYEE_NAME} is done with tasks\
+              ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+        for task in todos_json:
+            if str(task["userId"]) == user_id and task["completed"] == True:
+                print(f"\t{task['title']}")
+
+if __name__ == "__main__":
+    Get_Todo().todo()
+
